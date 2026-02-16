c67072dced006214918bb1b3b6536d50de1f0ae8
sagarsr19 <srs314848@gmail.com>
2026-02-16 07:16:49 +0000

    c4

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+def power(a, b):
+    """Raise a to the power of b"""
+    return a ** b
+
